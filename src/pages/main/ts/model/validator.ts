90858e94e8e2af73ad00e500bda295b2c043b654
Maahir Jain <maahirdjain@gmail.com>
2024-07-13 23:24:58 +0530

    Update isValid condition to exclude numbers less than -1000

--- src/pages/main/ts/model/validator.ts
+++ src/pages/main/ts/model/validator.ts
@@ -3,7 +3,7 @@ import { evaluate } from "mathjs";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Validator {
   /**
-   * Checks if a cell input is a real number less than 1000.
+   * Checks if a cell input is a real number less between -1000 and 1000 (exclusive).
    *
    * @param input the user's cell input
    * @returns true for a valid cell and false for an invalid cell
@@ -11,35 +11,21 @@ class Validator {
   public isCellValid(input: string): boolean {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const evaluatedInput: any = evaluate(input);
-    return typeof evaluatedInput === "number" && evaluatedInput < 1000;
+    return (
+      typeof evaluatedInput === "number" &&
+      !isNaN(+evaluatedInput) &&
+      evaluatedInput < 1000 &&
+      evaluatedInput > -1000
+    );
   }
 
   /**
-   * Checks if every cell in the input matrix contains a real number less than 1000.
+   * Checks if every cell in the input matrix contains a real number between -1000 and 1000 (exclusive).
    *
    * @param matrix the user's input matrix
    * @returns true if every cell in the matrix is valid and false otherwise
    */
   public isMatrixValid(matrix: string[][]): boolean {
     return matrix.every((row) => row.every((cell) => this.isCellValid(cell)));
-  }
-
-  /**
-   * Identifies cells that are not real numbers less than 1000 in the input matrix.
-   *
-   * @param matrix the user's input matrix
-   * @returns an array of indices of invalid cells
-   */
-  public getInvalidCells(matrix: string[][]): number[] {
-    const arr: number[] = [];
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix.length; j++) {
-        if (!this.isCellValid(matrix[i][j])) {
-          arr.push(matrix.length * i + j);
-        }
-      }
-    }
-
-    return arr;
   }
 }
